bde6d61c0e0e45c615dd175381512f12e3c7e9ba
axelmark <axelmark@mail.ru>
2021-11-22 20:22:24 +0300

    3. Реализация профессий в коде [#6837]

---
+++ src/main/java/ru/job4j/tracker/inheritance/Dentist.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.inheritance;
+
+import java.util.Date;
+
+public class Dentist extends Doctor {
+
+    private String tooth;
+
+    public Dentist(String name, String surname, String education, Date birthday,
+        String qualification, String tooth) {
+        super(name, surname, education, birthday, qualification);
+        this.tooth = tooth;
+    }
+
+    public String getTooth() {
+        return tooth;
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/inheritance/Diagnosis.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker.inheritance;
+
+public class Diagnosis {
+
+    private String diagnos1;
+    private String diagnos2;
+    private String diagnos3;
+
+    public Diagnosis(String diagnos1, String diagnos2, String diagnos3) {
+        this.diagnos1 = diagnos1;
+        this.diagnos2 = diagnos2;
+        this.diagnos3 = diagnos3;
+    }
+}
