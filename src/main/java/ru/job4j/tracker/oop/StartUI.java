256547543cfefcd9bdba7919a51b7ce1f50d85c0
axelmark <axelmark@mail.ru>
2021-11-24 13:42:02 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/oop/StartUI.java
+++ src/main/java/ru/job4j/tracker/oop/StartUI.java
@@ -8,7 +8,7 @@ public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        LocalDateTime dd = item.getCreated();
-        System.out.println(dd.format(formatter));
+        LocalDateTime dateTime = item.getCreated();
+        System.out.println(dateTime.format(formatter));
     }
 }
