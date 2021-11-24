5ab11ebcd36bca86d827e230f99a63ec313815b5
axelmark <axelmark@mail.ru>
2021-11-24 15:31:42 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/tracker/pojo/Library.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book java = new Book("Java", 8);
+        Book arrays = new Book("Arrays", 10);
+        Book methods = new Book("Method", 15);
+        Book cleanBook = new Book("Clean code", 20);
+        Book[] library = new Book[4];
+        library[0] = java;
+        library[1] = arrays;
+        library[2] = methods;
+        library[3] = cleanBook;
+        for (Book value : library) {
+            System.out.println(value.getName() + " - " + value.getCount());
+        }
+        Book temp = library[0];
+        library[0] = library[3];
+        library[3] = temp;
+        for (Book book : library) {
+            System.out.println(book.getName() + " - " + book.getCount());
+
+        }
+        for (int i = 0; i < library.length; i++) {
+            if (library[i].getName() == "Clean code") {
+                System.out.println(library[i].getName() + " - " + library[i].getCount());
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/pojo/Store.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.pojo;
+
+public class Store {
+
+    public static void main(String[] args) {
+        Product milk = new Product("Milk", 10);
+        Product bread = new Product("Bread", 4);
+        Product egg = new Product("Egg", 19);
+        Product[] prods = new Product[3];
+        prods[0] = milk;
+        prods[1] = bread;
+        prods[2] = egg;
+
+        for (int i = 0; i < prods.length; i++) {
+            System.out.println(prods[i].getName() + " - " + prods[i].getCount());
+        }
+    }
+}
