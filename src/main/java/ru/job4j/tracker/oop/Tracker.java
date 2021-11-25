6d739a736ed4be8faf59c3cf4c49cbf30bcea274
axelmark <axelmark@mail.ru>
2021-11-25 20:10:58 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/oop/Tracker.java
+++ src/main/java/ru/job4j/tracker/oop/Tracker.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker.oop;
 
+import java.util.Arrays;
+
 public class Tracker {
+
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
@@ -9,6 +12,23 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findByName(String name) {
+        Item[] current = findAll();
+        Item[] res = new Item[size];
+        int size = 0;
+        for (int i = 0; i < current.length; i++) {
+            if (name.equals(current[i].getName())) {
+                res[i] = current[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(res, size);
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
     }
 
     public Item findById(int id) {
---
+++ src/test/java/ru/job4j/tracker/oop/TrackerTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.tracker.oop;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+public class TrackerTest {
+
+    @Test
+    public void whenTestFindById() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item("Bug");
+        Item item = tracker.add(bug);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenTestFindAll() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        tracker.add(first);
+        tracker.add(second);
+        Item[] result = tracker.findAll();
+        assertThat(result[0].getName(), is(first.getName()));
+        assertThat(result[1].getName(), is(second.getName()));
+    }
+
+    @Test
+    public void whenTestFindByNameCheckArrayLength() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(new Item("First"));
+        tracker.add(new Item("Second"));
+        tracker.add(new Item("First"));
+        Item[] result = tracker.findByName(first.getName());
+        assertThat(result.length, is(3));
+    }
+
+    @Test
+    public void whenTestFindByNameCheckSecondItemName() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(new Item("First"));
+        tracker.add(new Item("Second"));
+        tracker.add(new Item("First"));
+        Item[] result = tracker.findByName(second.getName());
+        assertThat(result[1].getName(), is(second.getName()));
+    }
+}
