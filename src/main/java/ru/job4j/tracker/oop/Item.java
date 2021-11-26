9aafd675252401394044532f412317bec69d5503
axelmark <axelmark@mail.ru>
2021-11-26 03:55:48 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/oop/Item.java
+++ src/main/java/ru/job4j/tracker/oop/Item.java
@@ -8,6 +8,8 @@ public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter
+        .ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
     }
@@ -39,5 +41,14 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+            + "id=" + id
+            + ", name='" + name + '\''
+            + ", created=" + created.format(FORMATTER)
+            + '}';
     }
 }
--- src/main/java/ru/job4j/tracker/oop/StartUI.java
+++ src/main/java/ru/job4j/tracker/oop/StartUI.java
@@ -1,14 +1,9 @@
 package ru.job4j.tracker.oop;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
 
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        LocalDateTime dateTime = item.getCreated();
-        System.out.println(dateTime.format(formatter));
+        Item item = new Item(1, "Zhanna");
+        System.out.println(item);
     }
 }
