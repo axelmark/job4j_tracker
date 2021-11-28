c7e729db34a7c7d71eedd3618e07e6b3a17b5ab5
axelmark <axelmark@mail.ru>
2021-11-28 13:56:04 +0300

    2.3. Реализация класса StartUI. Добавление заявки [#500744 #233371]

--- src/main/java/ru/job4j/tracker/oop/StartUI.java
+++ src/main/java/ru/job4j/tracker/oop/StartUI.java
@@ -7,6 +7,7 @@ public class StartUI {
     public void init(Scanner scanner, Tracker tracker) {
         boolean run = true;
         while (run) {
+            System.out.println("=== Menu: ===");
             showMenu();
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
