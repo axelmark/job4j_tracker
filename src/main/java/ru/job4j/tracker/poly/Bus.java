729a8e9d1745039cf2e2abbe7539b49979143d5b
axelmark <axelmark@mail.ru>
2021-11-28 22:42:10 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/tracker/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passenger(int count) {
+
+    }
+
+    @Override
+    public int fill(int fuel) {
+        return 0;
+    }
+}
