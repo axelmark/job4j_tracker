05913a825ddb044ab77192576c1a161618d22786
axelmark <axelmark@mail.ru>
2021-11-29 11:19:04 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#181322]

--- src/main/java/ru/job4j/tracker/poly/Bus.java
+++ src/main/java/ru/job4j/tracker/poly/Bus.java
@@ -2,14 +2,20 @@ package ru.job4j.tracker.poly;
 
 public class Bus implements Transport {
 
+    private int count;
+    private int fuel;
+
+    public Bus(int count, int fuel) {
+        this.count = count;
+        this.fuel = fuel;
+    }
+
     @Override
     public void drive() {
-
     }
 
     @Override
     public void passenger(int count) {
-
     }
 
     @Override
