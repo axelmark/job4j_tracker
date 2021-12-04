538ec12d645d0445666a70b59e0c017f3be0d25c
axelmark <axelmark@mail.ru>
2021-12-04 21:37:35 +0300

    9.1 Зависимость от System.out [#33568]

--- src/test/java/ru/job4j/tracker/oop/StartUITest.java
+++ src/test/java/ru/job4j/tracker/oop/StartUITest.java
@@ -64,21 +64,4 @@ public class StartUITest {
         new StartUI(output).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
-
-//    @Test
-//    public void whenExit() {
-//        Output output = new StubOutput();
-//        Input in = new StubInput(
-//            new String[]{"0"}
-//        );
-//        Tracker tracker = new Tracker();
-//        UserAction[] actions = {
-//            new Exit()
-//        };
-//        new StartUI(output).init(in, tracker, actions);
-//        assertThat(output.toString(), is("Menu."
-//            + System.lineSeparator()
-//            + "0. Exit" + System.lineSeparator()
-//        ));
-//    }
 }
