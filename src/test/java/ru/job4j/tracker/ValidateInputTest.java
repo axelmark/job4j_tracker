de1943f0f01b439dcbff14871b097be1e17ece53
axelmark <axelmark@mail.ru>
2021-12-08 10:16:11 +0300

    update ValidateInputTest

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -7,14 +7,14 @@ import org.junit.Test;
 
 public class ValidateInputTest {
 
-//    @Test
-//    public void whenInvalidInput() {
-//        Output out = new StubOutput();
-//        Input in = new StubInput(
-//            new String[]{"0ne"}
-//        );
-//        ValidateInput input = new ValidateInput(out, in);
-//        int selected = input.askInt("Enter menu:");
-//        assertThat(selected, is(1));
-//    }
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+            new String[]{"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
 }
