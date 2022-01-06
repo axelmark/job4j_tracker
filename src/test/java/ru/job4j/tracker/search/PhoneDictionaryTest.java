662a991b81bc9fe011b1be6489a8af99cbeae081
axelmark <axelmark@mail.ru>
2022-01-06 14:19:02 +0300

    Person update

--- src/test/java/ru/job4j/tracker/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/tracker/search/PhoneDictionaryTest.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker.search;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
 
 import java.util.ArrayList;
 import org.junit.Test;
@@ -16,6 +17,16 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenNotFind() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+            new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("FInd");
+        assertThat(null, isEmptyOrNullString());
     }
 }
 
