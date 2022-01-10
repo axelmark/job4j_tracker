9f1d15279d96c68a3be7cb4f0d483527534cc71e
axelmark <axelmark@mail.ru>
2022-01-10 21:08:21 +0300

    1. Уникальные задачи. [#212653]

---
+++ src/main/java/ru/job4j/tracker/collection/Task.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker.collection;
+
+public class Task {
+
+    private String number;
+    private String description;
+
+    public Task(String number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
