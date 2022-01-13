55dac135f97698b81ddab509228889122863ab78
axelmark <axelmark@mail.ru>
2022-01-13 17:42:09 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/tracker/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev Sergeevich");
+        for (String key : map.keySet()) {
+            System.out.println(key + " " + map.get(key));
+        }
+    }
+}
