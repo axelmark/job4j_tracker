e2cfa9185192aaecae7cadefb52d4089e5575ada
axelmark <axelmark@mail.ru>
2022-01-30 12:03:09 +0300

    1.2. Функциональный интерфейс [#247497]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                tmp.add(start + "/" + el);
+
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        Collections.sort(orgs);
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        Collections.sort(orgs, Collections.reverseOrder());
+    }
+}
