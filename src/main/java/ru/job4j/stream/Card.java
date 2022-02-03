a023ee51f4ba22304f7ba9488c982507b012a2c9
axelmark <axelmark@mail.ru>
2022-02-03 18:59:19 +0300

    4. Optional в Stream API. [#279130]

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -2,40 +2,30 @@ package ru.job4j.stream;
 
 import java.util.stream.Stream;
 
+enum Suit {
+    Diamonds, Hearts, Spades, Clubs
+}
+
+enum Value {
+    V_6, V_7, V_8
+}
+
 public class Card {
 
-    private Suit[] suit;
-    private Value[] value;
+    private Suit suit;
+    private Value value;
 
-    public Suit[] getSuit() {
-        return suit;
-    }
-
-    public Value[] getValue() {
-        return value;
-    }
-
-    public enum Suit {
-        Diamonds, Hearts, Spades, Clubs
-    }
-
-    public enum Value {
-        V_6, V_7, V_8
-    }
-
-    public Card(Suit[] suit, Value[] value) {
+    public Card(Suit suit, Value value) {
         this.suit = suit;
         this.value = value;
     }
 
     public static void main(String[] args) {
-        Card card = new Card(
-            new Suit[]{Suit.Diamonds, Suit.Clubs, Suit.Hearts, Suit.Spades},
-            new Value[]{Value.V_6, Value.V_7, Value.V_8}
-        );
+        Suit[] suit = {Suit.Clubs, Suit.Diamonds, Suit.Hearts, Suit.Spades};
+        Value[] values = {Value.V_6, Value.V_7, Value.V_8};
 
-        Stream.of(card.getSuit())
-            .flatMap(level -> Stream.of(card.getValue())
+        Stream.of(suit)
+            .flatMap(level -> Stream.of(values)
                 .map(task -> level + " " + task))
             .forEach(System.out::println);
     }
