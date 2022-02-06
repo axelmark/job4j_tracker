83b9bb0cfb179ee2246187cf92f5ad9db1257474
axelmark <axelmark@mail.ru>
2022-02-07 01:21:25 +0300

    2. Аттестация [#49777 #261032]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,61 @@
+package ru.job4j.stream;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream
+            .flatMap(x -> x.getSubjects().stream())
+            .mapToInt(Subject::getScore)
+            .average()
+            .orElse(100D);
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream
+            .map(pupil -> new Tuple(
+                    pupil.getName(),
+                    pupil.getSubjects().stream()
+                        .mapToDouble(Subject::getScore)
+                        .average()
+                        .orElse(100D)
+                )
+            )
+            .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream
+            .flatMap(x -> x.getSubjects().stream())
+            .collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new,
+                Collectors.averagingDouble(Subject::getScore)))
+            .entrySet()
+            .stream()
+            .map(a -> new Tuple(a.getKey(), a.getValue()))
+            .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream
+            .map(pupil -> new Tuple(
+                    pupil.getName(),
+                    pupil.getSubjects().stream().mapToDouble(Subject::getScore).sum()
+                )
+            ).max((a, b) -> (int) Double.max(a.getScore(), b.getScore())).get();
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream
+            .flatMap(x -> x.getSubjects().stream())
+            .collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new,
+                Collectors.summingDouble(Subject::getScore)))
+            .entrySet()
+            .stream()
+            .map(a -> new Tuple(a.getKey(), a.getValue()))
+            .max((x, y) -> (int) Double.max(x.getScore(), y.getScore())).get();
+    }
+}
